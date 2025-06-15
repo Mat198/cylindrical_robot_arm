c7998ce517e77b2b62b7d214bf54b3160a9a732d
Mat198 <matheussousasf@gmail.com>
2025-06-15 16:56:53 -0300

    Function to plot trajectory with workspace

--- scripts/plot_graphs.py
+++ scripts/plot_graphs.py
@@ -10,3 +10,15 @@ def plotRobotWorkspace(points):
     ax.set_title('Área de Trabalho - Gerada pela cinemática direta')
     plt.tight_layout()
     plt.show()
+
+def plotRouteInWorkspace(origin, target, workspace):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(workspace[:, 0], workspace[:, 1], workspace[:, 2], s=1, alpha=0.5, c='blue')
+    ax.plot([origin[0], target[0]], [origin[1], target[1]], [origin[2], target[2]], c='red')
+    ax.set_xlabel('X (m)')
+    ax.set_ylabel('Y (m)')
+    ax.set_zlabel('Z (m)')
+    ax.set_title('Área de Trabalho com rota proposta')
+    plt.tight_layout()
+    plt.show()

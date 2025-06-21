358f9d31ea9c52978a3bb6671684334463f87a1e
Mat198 <matheussousasf@gmail.com>
2025-06-21 16:15:29 -0300

    Open scene during coppelia connection

--- scripts/connect.py
+++ scripts/connect.py
@@ -1,12 +1,20 @@
 import time
 from coppeliasim_zmqremoteapi_client import RemoteAPIClient
+import os
 
 def connect():
+
+    SCENE_PATH = "sim/cilyndric_robot_scene.ttt"
+    scenePath = os.path.abspath(SCENE_PATH)
+
     for attempt in range(20):
         try:
             client = RemoteAPIClient()
             sim = client.getObject('sim')
             print(f"[SUCCESS] Conectado ao CoppeliaSim")
+            result = sim.loadScene(scenePath)
+            if not result:
+                raise RuntimeError("Não foi possível abrir a cena do CoppeliaSim.")
             return sim
         except Exception:
             print(f"[WAITING] Tentando conectar... ({attempt + 1}/20)")

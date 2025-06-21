5e2cf16685756f457af3a787557b1079b9de96da
Mat198 <matheussousasf@gmail.com>
2025-06-21 19:29:47 -0300

    Openning scene on script run

--- scripts/connect.py
+++ scripts/connect.py
@@ -4,7 +4,7 @@ import os
 
 def connect():
 
-    SCENE_PATH = "sim/cilyndric_robot_scene.ttt"
+    SCENE_PATH = "../sim/cilyndric_robot_scene.ttt"
     scenePath = os.path.abspath(SCENE_PATH)
 
     for attempt in range(20):
@@ -12,11 +12,12 @@ def connect():
             client = RemoteAPIClient()
             sim = client.getObject('sim')
             print(f"[SUCCESS] Conectado ao CoppeliaSim")
-            result = sim.loadScene(scenePath)
-            if not result:
-                raise RuntimeError("Não foi possível abrir a cena do CoppeliaSim.")
             return sim
         except Exception:
             print(f"[WAITING] Tentando conectar... ({attempt + 1}/20)")
             time.sleep(1)
+        try:
+            sim.loadScene(scenePath)
+        except Exception:
+            raise RuntimeError("Não foi possível abrir a cena do CoppeliaSim.")
     raise TimeoutError("Não foi possível conectar ao CoppeliaSim.")
